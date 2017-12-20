1f61927cbdb0e079a3eb2581c2dd66c1dbc6a0e6
Saulo Alvarado Mateos <saulo.alvarado.contractor@bbva.com>
2017-12-20 19:18:19 +0000

    Cucumber moved to test folder

--- src/test/java/io/theam/cucumber/StepsDefinitions.java
+++ src/test/java/io/theam/cucumber/StepsDefinitions.java
@@ -1,10 +1,10 @@
-package io.theam;
+package io.theam.cucumber;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.theam.App;
 import io.theam.model.Customer;
-import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
